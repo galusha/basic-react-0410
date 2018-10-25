484b6d223237051eb914c5d5d4d9de78d22da70d
SeaSalt88 <seasalt88@gmail.com>
2018-10-25 09:23:02 +0300

    HT4.1 Added Form for adding a new Comment

--- src/reducer/forms.js
+++ src/reducer/forms.js
@@ -10,7 +10,6 @@ const forms = {
 export default (state = forms, action) => {
   const { type, payload } = action
 
-  console.log('payload', payload)
   switch (type) {
     case CHANGE_COMMENT_FORM:
       return { ...state, comment: { ...state.comment, ...payload } }
